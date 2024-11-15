736ebf118cf8829917600716a70e165e32787c08
Xharry01X <harshitsingh50621@gmail.com>
2024-11-15 14:31:22 +0530

    small fix..

--- profileOther/src/App.tsx
+++ profileOther/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 
 import './App.css'
 import UserManagement from './component/UserManagement'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
